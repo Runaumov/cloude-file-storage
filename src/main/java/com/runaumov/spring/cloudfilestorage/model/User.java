79e94b78b41f32ba8474e4270ca7132367c06042
Runaumov <casrunau@gmail.com>
2025-08-14 18:49:34 +0300

    feat(model): add User entity

--- src/main/java/com/runaumov/spring/cloudfilestorage/model/User.java
+++ src/main/java/com/runaumov/spring/cloudfilestorage/model/User.java
@@ -1,4 +1,25 @@
 package com.runaumov.spring.cloudfilestorage.model;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "users")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private long id;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "password")
+    private String password;
+
+
 }
---
+++ src/main/resources/import.sql
@@ -0,0 +1,2 @@
+INSERT INTO users (username, password) VALUES ('Pupa', '123');
+INSERT INTO users (username, password) VALUES ('Lupa', '456')
