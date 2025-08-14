36c63aa4a7c06e9245b57ead2bf206fb9577707e
Runaumov <casrunau@gmail.com>
2025-08-14 19:28:31 +0300

    feat(config): create SecurityConfig

--- src/main/java/com/runaumov/spring/cloudfilestorage/config/SecurityConfig.java
+++ src/main/java/com/runaumov/spring/cloudfilestorage/config/SecurityConfig.java
@@ -1,4 +1,12 @@
 package com.runaumov.spring.cloudfilestorage.config;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+
+
 }
