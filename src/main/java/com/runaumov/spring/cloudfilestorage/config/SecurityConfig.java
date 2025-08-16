e88837d9524ce57ce222355100a16843d71aee67
Runaumov <casrunau@gmail.com>
2025-08-16 19:21:51 +0300

    feat(controller): create AuthController

--- src/main/java/com/runaumov/spring/cloudfilestorage/config/SecurityConfig.java
+++ src/main/java/com/runaumov/spring/cloudfilestorage/config/SecurityConfig.java
@@ -4,14 +4,9 @@ import com.runaumov.spring.cloudfilestorage.service.UserEntityDetailService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -20,29 +15,10 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class SecurityConfig {
 
-    private final UserEntityDetailService userEntityDetailService;
-
-    @Autowired
-    public SecurityConfig(UserEntityDetailService userEntityDetailService) {
-        this.userEntityDetailService = userEntityDetailService;
-    }
-
     @Bean
     public PasswordEncoder passwordEncoder() {
         return NoOpPasswordEncoder.getInstance();
     }
-
-//    @Bean
-//    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
-//        return config.getAuthenticationManager();
-//    }
-//
-//    @Bean
-//    public AuthenticationProvider authenticationProvider(UserEntityDetailService userEntityDetailService) {
-//        DaoAuthenticationProvider provider = new DaoAuthenticationProvider(userEntityDetailService);
-//        provider.setPasswordEncoder(passwordEncoder());
-//        return provider;
-//    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
