06cd58db9df10652e966bd214cfb9da77aae4614
Runaumov <casrunau@gmail.com>
2025-10-01 20:48:54 +0300

    fix(controller): add annotation to ResourceSearchController

--- src/main/java/com/runaumov/spring/cloudfilestorage/controller/ResourceSearchController.java
+++ src/main/java/com/runaumov/spring/cloudfilestorage/controller/ResourceSearchController.java
@@ -5,6 +5,7 @@ import com.runaumov.spring.cloudfilestorage.service.ResourceSearchService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -15,6 +16,7 @@ public class ResourceSearchController {
 
     private final ResourceSearchService resourceSearchService;
 
+    @GetMapping
     public ResponseEntity<ResourceResponseDto> searchResource(@RequestParam String query) {
         return ResponseEntity.ok(resourceSearchService.searchResource(query));
     }
