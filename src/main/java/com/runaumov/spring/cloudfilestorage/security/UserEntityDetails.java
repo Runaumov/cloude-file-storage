91a02ca1cea079ca0d735cb0f864a96f6d9e0e0c
Runaumov <casrunau@gmail.com>
2025-11-28 21:09:36 +0300

    feat(config): add redis

--- src/main/java/com/runaumov/spring/cloudfilestorage/security/UserEntityDetails.java
+++ src/main/java/com/runaumov/spring/cloudfilestorage/security/UserEntityDetails.java
@@ -3,7 +3,6 @@ package com.runaumov.spring.cloudfilestorage.security;
 import com.runaumov.spring.cloudfilestorage.entity.UserEntity;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
-
 import java.util.Collection;
 import java.util.List;
 
