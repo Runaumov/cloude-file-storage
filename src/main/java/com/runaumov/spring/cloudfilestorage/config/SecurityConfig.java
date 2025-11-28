5b05e3157d4e3f57bf7dbd8d31d1c82613ce5d16
Runaumov <casrunau@gmail.com>
2025-11-28 20:42:54 +0300

    feat(cinfig): add redis

--- src/main/java/com/runaumov/spring/cloudfilestorage/config/SecurityConfig.java
+++ src/main/java/com/runaumov/spring/cloudfilestorage/config/SecurityConfig.java
@@ -10,6 +10,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -67,7 +68,11 @@ public class SecurityConfig {
                         .invalidateHttpSession(true)
                         .deleteCookies("JSESSIONID")
                         .permitAll())
-                .addFilterAt(jsonAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+                .addFilterAt(jsonAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)
+                        .maximumSessions(1)
+                        .maxSessionsPreventsLogin(false));
 
         return httpSecurity.build();
     }
