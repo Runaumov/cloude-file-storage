b04d9118e31a54f1fb96d9d7ffdde2d86146ff02
Runaumov <casrunau@gmail.com>
2025-12-02 21:47:38 +0300

    refactor(service): refactor ResourceMoveService

--- questions.md
+++ questions.md
@@ -12,7 +12,7 @@ DirectoryService +
 ResourceDeleteService +
 ResourceDownloadService +
 ResourceInfoService +
-ResourceMoveService -
+ResourceMoveService +
 ResourceSearchService -
 UploadFileService -
 Создание пустой папки-
--- src/main/java/com/runaumov/spring/cloudfilestorage/service/ResourceMoveService.java
+++ src/main/java/com/runaumov/spring/cloudfilestorage/service/ResourceMoveService.java
@@ -3,7 +3,9 @@ package com.runaumov.spring.cloudfilestorage.service;
 import com.runaumov.spring.cloudfilestorage.dto.PathComponents;
 import com.runaumov.spring.cloudfilestorage.dto.ResourceResponseDto;
 import com.runaumov.spring.cloudfilestorage.dto.ResourceResponseDtoFactory;
+import com.runaumov.spring.cloudfilestorage.exception.InvalidPathException;
 import com.runaumov.spring.cloudfilestorage.util.MinioUtils;
+import com.runaumov.spring.cloudfilestorage.util.MinioValidator;
 import io.minio.*;
 import io.minio.messages.Item;
 import lombok.RequiredArgsConstructor;
@@ -15,61 +17,65 @@ public class ResourceMoveService {
     private final MinioStorageService minioStorageService;
     private final PathParserService pathParserService;
 
-    public ResourceResponseDto resourceMove(String from, String to) {
+    public ResourceResponseDto resourceMove(String oldPath, String newPath) {
+
+        checkOldNewPaths(oldPath, newPath);
 
         return MinioUtils.handleMinioException(() -> {
-            if (!isDirectory(from)) {
-                String targetPath = resolveConflict(to);
+            if (!pathParserService.isDirectory(oldPath)) {
+                MinioUtils.handleMinioException(() -> minioStorageService.getStatObject(oldPath),
+                        "File not found: " + oldPath);
 
-                minioStorageService.copyObject(targetPath, from);
-                minioStorageService.deleteItemForPath(from);
+                String targetPath = resolveConflict(newPath);
+
+                minioStorageService.copyObject(targetPath, oldPath);
+                minioStorageService.deleteItemForPath(oldPath);
 
                 StatObjectResponse statObject = minioStorageService.getStatObject(targetPath);
                 PathComponents pathComponents = pathParserService.parsePath(targetPath);
 
                 return ResourceResponseDtoFactory.createDtoFromStatObject(statObject, pathComponents);
             } else {
-                String normalizedFrom = pathParserService.normalizePath(from);
-                String normalizedTo = pathParserService.normalizePath(to);
+                MinioValidator.verificationDirectory(
+                        oldPath,
+                        () -> minioStorageService.getStatObject(oldPath),
+                        () -> minioStorageService.listDirectoryItems(oldPath, false),
+                        "Folder not found: " + oldPath);
 
-                var results = minioStorageService.listDirectoryItems(normalizedFrom, true);
+                var results = minioStorageService.listDirectoryItems(oldPath, true);
 
                 for (Result<Item> result : results) {
                     Item item = result.get();
                     String oldKey = item.objectName();
-                    String newKeyBase = normalizedTo + oldKey.substring(normalizedFrom.length());
+                    String newKeyBase = newPath + oldKey.substring(oldPath.length());
                     String newKey = resolveConflict(newKeyBase);
 
                     minioStorageService.copyObject(newKey, oldKey);
                     minioStorageService.deleteItemForPath(oldKey);
                 }
 
-                PathComponents pathComponents = pathParserService.parsePath(to);
+                PathComponents pathComponents = pathParserService.parsePath(newPath);
                 return ResourceResponseDtoFactory.createDtoFromPathComponents(pathComponents);
             }
-        }, "Failed to move resources from: " + from + " to: " + to);
-    }
-
-    private boolean isDirectory(String path) {
-        try {
-            return minioStorageService.listDirectoryItems(pathParserService.normalizePath(path), false).iterator().hasNext();
-        } catch (Exception e) {
-            return false;
-        }
+        }, "Failed to move resources from: " + oldPath + " to: " + newPath);
     }
 
     private String resolveConflict(String path) {
-        String pathWithSuffix = path;
+        String resolvedPath = path;
         int copyIndex = 1;
 
-        while (checkObjectExists(pathWithSuffix)) {
-            pathWithSuffix = appendCopySuffix(path, copyIndex);
-            copyIndex++;
+        while (checkObjectExists(resolvedPath)) {
+            resolvedPath = appendCopySuffix(path, copyIndex++);
         }
-        return pathWithSuffix;
+        return resolvedPath;
     }
 
     private String appendCopySuffix(String path, int copyIndex) {
+        if (pathParserService.isDirectory(path)) {
+            String pathWithoutSlash = path.substring(0, path.length() - 1);
+            return pathWithoutSlash + "_copy" + copyIndex + '/';
+        }
+
         int dotIndex = path.lastIndexOf('.');
         if (dotIndex > 0) {
             return path.substring(0, dotIndex) + "_copy" + copyIndex + path.substring(dotIndex);
@@ -83,5 +89,18 @@ public class ResourceMoveService {
                 () -> minioStorageService.objectExist(path),
                 "Failed to check object existence: " + path
         );
+    }
+
+    private void checkOldNewPaths(String oldPath, String newPath) {
+        if (pathParserService.isDirectory(oldPath)) {
+            if (oldPath.equals(newPath)) {
+                throw new InvalidPathException("Cannot move directory into itself: " + oldPath);
+            }
+            if (newPath.startsWith(oldPath)) {
+                throw new InvalidPathException(
+                        "Cannot move directory into its own subdirectory: " + newPath
+                );
+            }
+        }
     }
 }
