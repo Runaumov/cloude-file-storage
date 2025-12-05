78906a2c9b4b3b885620be1822815b7fd87ec95f
Runaumov <casrunau@gmail.com>
2025-12-05 21:56:34 +0300

    refactor(servcie): change ResourceDeleteService

--- src/main/java/com/runaumov/spring/cloudfilestorage/service/ResourceDeleteService.java
+++ src/main/java/com/runaumov/spring/cloudfilestorage/service/ResourceDeleteService.java
@@ -9,22 +9,33 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class ResourceDeleteService {
     private final MinioStorageService minioStorageService;
+    private final AuthenticationService authenticationService;
+    private final UserPathService userPathService;
+    private final PathParserService pathParserService;
+
 
     public void deleteResource(String path) {
-        if (!path.isEmpty()) {
-            MinioValidator.verificationDirectory(
-                    path,
-                    () -> minioStorageService.getStatObject(path),
-                    () -> minioStorageService.listDirectoryItems(path, false),
-                    "Folder not found: " + path
-            );
+        Long userId = authenticationService.getCurrentUserId();
+        String userPath = userPathService.addUserPrefix(userId, path);
+
+        if (pathParserService.isDirectory(userPath)) {
+            if (!userPath.equals(userPathService.getUserPrefix(userId))) {
+                MinioValidator.verificationDirectory(
+                        userPath,
+                        () -> minioStorageService.getStatObject(userPath),
+                        () -> minioStorageService.listDirectoryItems(userPath, false),
+                        "Folder not found: " + path
+                );
+            }
+        } else {
+            MinioUtils.handleMinioException(() -> minioStorageService.getStatObject(userPath), "Folder not found: " + path);
         }
 
         MinioUtils.handleMinioException(() -> {
-            if (path.endsWith("/")) { // TODO : переделать, подумать о централизованном методе
-                minioStorageService.deletePathRecursive(path);
+            if (pathParserService.isDirectory(userPath)) {
+                minioStorageService.deletePathRecursive(userPath);
             } else {
-                minioStorageService.deleteItemForPath(path);
+                minioStorageService.deleteItemForPath(userPath);
             }
             return null; // TODO можно убрать null
         }, "Failed to delete resource: " + path);
