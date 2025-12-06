172a8ac557f3d769a866bae522a7f62c72fe4563
Runaumov <casrunau@gmail.com>
2025-12-06 13:30:24 +0300

    feat(servcie): add UserContextService

---
+++ src/main/java/com/runaumov/spring/cloudfilestorage/service/UserContextService.java
@@ -0,0 +1,31 @@
+package com.runaumov.spring.cloudfilestorage.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserContextService {
+    private final AuthenticationService authenticationService;
+    private final UserPathService userPathService;
+
+    public Long getCurrentUserId() {
+        return authenticationService.getCurrentUserId();
+    }
+
+    public String getUserPrefix() {
+        return userPathService.getUserPrefix(getCurrentUserId(););
+    }
+
+    public String addUserPrefix(String path) {
+        return userPathService.addUserPrefix(getCurrentUserId(), path);
+    }
+
+    public String removeUserPrefix(String path) {
+        return userPathService.removeUserPrefix(getCurrentUserId(), path);
+    }
+
+    public boolean isUserRoot(String path) {
+        return path.equals(getUserPrefix());
+    }
+}
